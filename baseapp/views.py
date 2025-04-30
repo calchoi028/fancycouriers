cc80886251476bce3324381421636c059461b450
ccchoi2 <ccchoi@ualberta.ca>
2025-04-30 15:41:37 -0600

    work on home page

---
+++ baseapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def Home(request):
+    return render(request, 'home.html')
+
+def Shop(request):
+    return render(request, 'shop.html')
